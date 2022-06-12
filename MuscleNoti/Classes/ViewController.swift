c310ec684e64353e8060f6ee018242e24026ec2a
Giorgi Shamugia <giorgi9609@gmail.com>
2022-06-13 02:06:47 +0400

    - done

--- MuscleNoti/Classes/ViewController.swift
+++ MuscleNoti/Classes/ViewController.swift
@@ -48,14 +48,15 @@ class ViewController: UIViewController {
         MPVolumeView.setVolume(0.7)
         
         trigerNotification(interVal: timeInterval - 1)
-        MNSounds.shared.playSound(.success, atTime: timeInterval) //time is a TimeInterval after which the audio will start
+//        MNSounds.shared.playSound(.success, atTime: timeInterval) //time is a TimeInterval after which the audio will start
     }
     
     func trigerNotification(interVal: TimeInterval) {
         let content = UNMutableNotificationContent()
-        content.title = "stop"
-        content.subtitle = "tap to stop sound"
-        content.sound = .none
+        content.interruptionLevel = .timeSensitive
+        content.title = "Alarm is playing"
+        content.subtitle = "Tap to stop sound"
+        content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue:"beastie_boys_sabotage_noti.mp3"))
 
         // show this notification five seconds from now
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: interVal, repeats: false)
